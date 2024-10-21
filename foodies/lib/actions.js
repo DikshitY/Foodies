f205404c9496d37fd306a2379050acbbfd341d94
Dikshit <137251277+DikshitY@users.noreply.github.com>
2024-10-21 06:53:26 +0000

    change addMeal action

--- foodies/lib/actions.js
+++ foodies/lib/actions.js
@@ -1,9 +1,15 @@
 "use server"
 
 import { redirect } from "next/navigation"
+import { revalidatePath } from "next/cache";
+
 import { saveMeal } from "./meals"
 
-export async function addMeal(formData){
+function isInavlidInput(text) {
+    return !text || text.trim() === "";
+}
+
+export async function addMeal(prevState, formData){
     const meal = {
         title: formData.get("title"),
         summary: formData.get("summary"),
@@ -13,6 +19,22 @@ export async function addMeal(formData){
         creator_email: formData.get("email"),
     }
 
+    if (
+        isInavlidInput(meal.title) ||
+        isInavlidInput(meal.summary) ||
+        isInavlidInput(meal.instructions) ||
+        isInavlidInput(meal.creator) ||
+        isInavlidInput(meal.creator_email) ||
+        !meal.creator_email.includes("@") ||
+        !meal.image ||
+        meal.image.size === 0
+    ) {
+        return {
+            message: "Invalid Input.",
+        };
+    }
+
     await saveMeal(meal) 
+    revalidatePath("/meals");
     redirect("/meals")
 }
